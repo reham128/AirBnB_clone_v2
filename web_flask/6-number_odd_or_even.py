496802f462387c9206f194728a01ac1f0f1007b1
Reham <r_nour30@yahoo.com>
2024-04-25 16:43:49 -0700

    up

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -49,5 +49,13 @@ def number_template(n):
     return (render_template("5-number.html", n=n))
 
 
+@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+def number_odd_or_even(n):
+    '''To display an html page if n i int'''
+    if isinstance(n, int):
+        return (render_template("6-number_odd_or_even.html", n=n,
+                                odd_even='even' if n % 2 == 0 else 'odd'))
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
