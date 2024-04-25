a38b7e8df7dfbeeeffb5fde959c7e3a16a918962
Reham <r_nour30@yahoo.com>
2024-04-25 16:02:44 -0700

    up

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,9 +10,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """
-    /: display “Hello HBNB!”
-    """
+    ''' display “Hello HBNB!” '''
     return ('Hello HBNB!')
 
 
