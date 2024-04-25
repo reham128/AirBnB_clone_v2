99992566e78bb6991acb084ac00788066b334772
Reham <r_nour30@yahoo.com>
2024-04-25 15:44:06 -0700

    up

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""a script that starts a Flask web application"""
+"""
+a script that starts a Flask web application
+"""
 from flask import Flask
 from werkzeug.utils import url_quote
 
@@ -14,4 +16,4 @@ def hello_hbnb():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000')
+    app.run(host='0.0.0.0', port=5000)
