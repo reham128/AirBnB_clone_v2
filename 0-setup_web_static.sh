94e988c0c2d0f341c902f42e2d4c862fd372fd6a
Reham <r_nour30@yahoo.com>
2024-05-04 06:41:53 -0700

    1st deploy

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#This is a script to setup web server for deploy of web_static
+
+apt-get update
+apt-get install -y nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+sudo echo "<html>
+<head></head>
+<body>Holberton School</body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+sudo service nginx restart
