9e024e6e750d6c4af0908dbec67d88ba21899d01
Reham <r_nour30@yahoo.com>
2024-05-05 01:52:49 -0700

    try

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,35 @@
+##!/usr/bin/python3
+'''Fabric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers'''
+from fabric.api import *
+import os
+
+env.hosts = ["54.196.196.104", "100.25.102.27"]
+env.user = "ubuntu"
+env.key_filename = '~/.ssh/id_rsa'
+
+
+def do_deploy(archive_path):
+    '''to deploy web files to servers web-01, web-02'''
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        archive_name = os.path.basename(archive_path)
+        archive_no_ext = os.path.splitext(archive_name)[0]
+
+        put(archive_path, "/tmp/")
+        run("mkdir -p /data/web_static/releases/{}/".format(archive_no_ext))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(archive_name, archive_no_ext))
+        run("rm /tmp/{}".format(archive_name))
+        run("mv /data/web_static/releases/{}/web_static/* /data/web_static
+            /releases/{}/".format(archive_no_ext, archive_no_ext))
+        run("rm -rf /data/web_static/releases/{}/web_static"
+            .format(archive_no_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(archive_no_ext))
+        return True
+    except Exception as e:
+        print(f"Error: {e}")
+        return False
