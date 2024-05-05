d5acd7bc1361aa8dd3680d152752dc5eca99f422
Reham <r_nour30@yahoo.com>
2024-05-05 01:16:56 -0700

    up

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''fabric script to generate .tgz archive from the contents of
+web_static folder'''
+from fabric.api import *
+from datetime import datetime
+import os
+
+
+def do_pack():
+    '''to  generate .tgz archive of web_static folder'''
+    try:
+        local('sudo mkdir -p versions')
+        time_str = datetime.now.strftime('5Y%m%d%H%M%S')
+        local('sudo versions/web_static_{}.tgz web_static'.format(time_str))
+        dir_path = 'versions/web_static_{}.tgz'.format(time_str)
+        dir_size = os.path.getsize(dir_path)
+        print('web_static packed: {} -> {}Bytes'.format(dir_path, dir_size))
+
+        return ('versions/web_static_{}.tgz'.format(time_str))
+    except:
+        return (None)
