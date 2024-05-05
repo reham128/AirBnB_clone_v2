698e4a6e49100c19eafa7f32a83084f9b90f984c
Reham <r_nour30@yahoo.com>
2024-05-05 02:17:00 -0700

    update now

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,5 +1,5 @@
 ##!/usr/bin/python3
-'''Fabric script (based on the file 1-pack_web_static.py)'''
+'''Fabric script (based on the file 1-pack_web_static.py) use do_deploy'''
 from fabric.api import *
 import os
 
