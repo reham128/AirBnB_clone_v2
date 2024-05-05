4c6a2d41b99d4a096e534ce1205c984ce821c61b
Reham <r_nour30@yahoo.com>
2024-05-05 04:33:04 -0700

    up

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -28,25 +28,28 @@ def do_pack():
 
 def do_deploy(archive_path):
     """distributes an archive to the web servers"""
-    if os.path.exists(archive_path):
-        archived_file = archive_path[9:]
-        newest_version = "/data/web_static/releases/" + archived_file[:-4]
-        archived_file = "/tmp/" + archived_file
+    if not os.path.exists(archive_path):
+        return False
+    try:
+        archive_name = os.path.basename(archive_path)
+        archive_no_ext = os.path.splitext(archive_name)[0]
+
         put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(newest_version))
-        run("sudo tar -xzf {} -C {}/".format(archived_file,
-                                             newest_version))
-        run("sudo rm {}".format(archived_file))
-        run("sudo mv {}/web_static/* {}".format(newest_version,
-                                                newest_version))
-        run("sudo rm -rf {}/web_static".format(newest_version))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(newest_version))
-
-        print("New version deployed!")
+        run("mkdir -p /data/web_static/releases/{}/".format(archive_no_ext))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(archive_name, archive_no_ext))
+        run("rm /tmp/{}".format(archive_name))
+        run("mv /data/web_static/releases/{}/web_static/* /data/web_static"
+            "/releases/{}/".format(archive_no_ext, archive_no_ext))
+        run("rm -rf /data/web_static/releases/{}/web_static"
+            .format(archive_no_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+            .format(archive_no_ext))
         return True
-
-    return False
+    except Exception as e:
+        print(f"Error deploying archive: {e}")
+        return False
 
 
 def deploy():
